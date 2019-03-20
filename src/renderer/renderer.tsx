cc074a3a7ea31073440317ca8d1c295049feede5
Ankur Sheel <ankur.sheel@gmail.com>
2019-03-20 16:45:30 +1300

    Fix for reload not working

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { HashRouter as Router, Link, Route } from "react-router-dom";
 import { Routes } from "./routes";
 import "./style.scss";
 
