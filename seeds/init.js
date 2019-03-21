3a09d1454772616dab90fd8450bc083c79676a15
Ankur Sheel <ankur.sheel@gmail.com>
2019-03-21 13:46:43 +1300

    seed 1000 entries using faker

--- seeds/init.js
+++ seeds/init.js
@@ -1,46 +1,41 @@
+const faker = require("faker");
+const moment = require("moment");
+
+faker.seed(123);
+
+const createFakeEntry = () => {
+    let fakeEntry = {
+        date: moment(
+            faker.date.between("2000-01-01", "2017-01-01"),
+            "YYYY-MM"
+        ).format("YYYY-MM-01"),
+        name: faker.name.firstName() + faker.name.lastName(),
+        type: faker.random.arrayElement(["Asset", "Debt"]),
+    };
+    if (fakeEntry.type === "Asset") {
+        fakeEntry.amount = faker.finance.amount(500, 1000, 2);
+    } else {
+        fakeEntry.amount = faker.random.number(-500, 0, 2);
+    }
+    return fakeEntry;
+};
+
 exports.seed = function(knex, Promise) {
     // Deletes ALL existing entries
     return knex("networth")
         .del()
-        .then(function() {
-            // Inserts seed entries
-            return knex("networth").insert([
-                {
-                    date: "2019-03-01",
-                    name: "Checking Account",
-                    type: "Asset",
-                    amount: 100,
-                },
-                {
-                    date: "2019-03-01",
-                    name: "Savings Account",
-                    type: "Asset",
-                    amount: 400,
-                },
-                {
-                    date: "2019-03-01",
-                    name: "Credit Card",
-                    type: "Debt",
-                    amount: -200,
-                },
-                {
-                    date: "2019-04-01",
-                    name: "Checking Account",
-                    type: "Asset",
-                    amount: 200,
-                },
-                {
-                    date: "2019-04-01",
-                    name: "Savings Account",
-                    type: "Asset",
-                    amount: 500,
-                },
-                {
-                    date: "2019-04-01",
-                    name: "Credit Card",
-                    type: "Debt",
-                    amount: -100,
-                },
-            ]);
+        .then(async function() {
+            const fakeEntries = [];
+            const desiredFakeEntries = 1000;
+            for (let i = 1; i <= desiredFakeEntries; i++) {
+                fakeEntries.push(createFakeEntry());
+                if (fakeEntries.length % 100 === 0) {
+                    await knex("networth").insert(fakeEntries);
+                    console.log(fakeEntries.length);
+                    fakeEntries.splice(0, fakeEntries.length);
+                    console.log(fakeEntries.length);
+                }
+            }
+            await knex("networth").insert(fakeEntries);
         });
 };
