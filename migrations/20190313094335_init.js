676c58fbd62f23abb459d001960a9a3f3ec35e0c
Ankur Sheel <ankur.sheel@gmail.com>
2019-03-31 09:16:38 +1300

    add id to networth table

--- migrations/20190313094335_init.js
+++ migrations/20190313094335_init.js
@@ -1,5 +1,6 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable("networth", function(t) {
+        t.increments("id");
         t.date("date").notNullable();
         t.string("name").notNullable();
         t.enum("type", ["Asset", "Debt"]).notNullable();
