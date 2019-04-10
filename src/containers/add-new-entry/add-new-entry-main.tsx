d511a0aa7709555c72cf71bf13e3895d9308d858
Ankur Sheel <ankur.sheel@gmail.com>
2019-04-10 14:57:54 +1200

    use material ui

--- src/containers/add-new-entry/add-new-entry-main.tsx
+++ src/containers/add-new-entry/add-new-entry-main.tsx
@@ -1,11 +1,32 @@
 import * as moment from "moment";
 import * as React from "react";
 import { useState } from "react";
-import { Button } from "../../Components/button";
-import { Input } from "../../Components/input";
-import { Select } from "../../Components/select";
+import { DatePicker, MuiPickersUtilsProvider, MaterialUiPickersDate } from "material-ui-pickers";
+import DateFnsUtils from "@date-io/moment";
+import Button from "@material-ui/core/Button";
+import {
+    TextField,
+    InputAdornment,
+    FormControl,
+    createStyles,
+    Select,
+    MenuItem,
+    InputLabel,
+    withStyles,
+    WithStyles,
+} from "@material-ui/core";
 
-export const AddNewEntryMain = () => {
+const styles = () =>
+    createStyles({
+        FormControl: {
+            width: 500,
+        },
+        Button: {
+            margin: 20,
+        },
+    });
+
+const AddNewEntryMainForm = ({ classes }: WithStyles<typeof styles>) => {
     const typeOptions = ["Asset", "Debt"];
 
     const accountNames = [
@@ -25,64 +46,126 @@ export const AddNewEntryMain = () => {
         "ANZ Credit Card",
         "Amex Credit Card",
     ];
-    const buttonStyles = {
-        margin: "10px 10px 10px 10px",
-    };
     const [accountName, setAccountName] = useState("");
     const [type, setType] = useState("");
     const [amount, setAmount] = useState<number>(0);
-    const [date, setDate] = useState("");
+    const [date, setDate] = useState<moment.Moment>(moment());
+    const [submitSuccess, setSubmitSuccess] = useState(false);
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
-        console.log(`${date} ${accountName} ${type} ${amount}`);
+
+        if (validateForm()) {
+            const submitSuccess: boolean = await submitForm();
+            setSubmitSuccess(submitSuccess);
+        }
     };
+
     const clearForm = () => {
         setAccountName("");
-        setDate("");
+        setDate(moment());
         setAmount(0);
         setType("");
     };
 
+    const validateForm = (): boolean => {
+        return true;
+    };
+
+    const submitForm = async (): Promise<boolean> => {
+        console.log(`${date} ${accountName} ${type} ${amount}`);
+        return true;
+    };
+
+    const handleAccountSelected = (e: React.ChangeEvent<HTMLSelectElement>) => setAccountName(e.target.value);
+
+    const handleTypeSelected = (e: React.ChangeEvent<HTMLSelectElement>) => setType(e.target.value);
+
     return (
-        <form className="container" onSubmit={handleSubmit}>
-            <Input
-                label="Date"
-                value={date}
-                type="text"
-                name="date"
-                placeholder="Mar 2019"
-                handleChange={(e: React.ChangeEvent<HTMLInputElement>) => setDate(e.target.value)}
-            />
-            <Select
-                title="Account"
-                name="name"
-                options={accountNames}
-                value={accountName}
-                placeholder={"Select Account"}
-                handleChange={(e: React.ChangeEvent<HTMLSelectElement>) => setAccountName(e.target.value)}
-            />
-            <Select
-                title="Type"
-                name="type"
-                options={typeOptions}
-                value={type}
-                placeholder={"Select Type"}
-                handleChange={(e: React.ChangeEvent<HTMLSelectElement>) => setType(e.target.value)}
-            />
-            <Input
-                label="Amount"
-                value={amount}
-                type="number"
-                name="amount"
-                placeholder="0"
-                handleChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    const value = e.target.value;
-                    value ? setAmount(parseInt(value)) : null;
-                }}
-            />
-            <Button isPrimary={true} type={"submit"} title={"Submit"} style={buttonStyles} />
-            <Button isPrimary={false} action={clearForm} type={"secondary"} title={"Clear"} style={buttonStyles} />
+        <form onSubmit={handleSubmit} noValidate={true} autoComplete="off">
+            <div className="container">
+                {submitSuccess && (
+                    <div className="alert alert-info" role="alert">
+                        The form was successfully submitted!
+                    </div>
+                )}
+                <FormControl>
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <DatePicker
+                            className={classes.FormControl}
+                            label="Select Date"
+                            views={["year", "month"]}
+                            value={date}
+                            onChange={(d: MaterialUiPickersDate) => setDate(moment(d).startOf("month"))}
+                            onMonthChange={setDate}
+                            showTodayButton={true}
+                            todayLabel={"Current Month"}
+                            disableFuture
+                        />
+                    </MuiPickersUtilsProvider>
+                </FormControl>
+                <br />
+                <br />
+                <FormControl>
+                    <InputLabel>Select Account</InputLabel>
+                    <Select
+                        className={classes.FormControl}
+                        value={accountName}
+                        name="account"
+                        onChange={handleAccountSelected}>
+                        {accountNames.map(a => (
+                            <MenuItem key={a} value={a}>
+                                {a}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <br />
+                <br />
+                <FormControl>
+                    <InputLabel>Select Type</InputLabel>
+                    <Select
+                        className={classes.FormControl}
+                        value={type}
+                        name="type"
+                        onChange={handleTypeSelected}
+                        autoWidth>
+                        {typeOptions.map(t => {
+                            return (
+                                <MenuItem key={t} value={t}>
+                                    {t}
+                                </MenuItem>
+                            );
+                        })}
+                    </Select>
+                </FormControl>
+                <br />
+                <br />
+                <TextField
+                    id="amount"
+                    className={classes.FormControl}
+                    variant="outlined"
+                    label="Amount"
+                    type="Number"
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                    }}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        const value = e.target.value;
+                        value ? setAmount(parseInt(value)) : null;
+                    }}
+                    placeholder={"Please enter the amount"}
+                />
+                <br />
+                <Button className={classes.Button} type="submit" variant="contained" color="primary">
+                    Submit
+                </Button>
+                <Button className={classes.Button} type="reset" variant="contained" color="default" onClick={clearForm}>
+                    Reset
+                </Button>
+            </div>
         </form>
     );
 };
+
+export const AddNewEntryMain = withStyles(styles)(AddNewEntryMainForm);
