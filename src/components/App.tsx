ef4b0838494b4c03ec957adfe089051e8b2fd8a1
Ankur Sheel <ankur.sheel@gmail.com>
2019-04-10 15:45:24 +1200

    create an app component

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { HashRouter as Router } from "react-router-dom";
+import { Routes } from "../routes/routes";
+
+export const App = () => (
+    <Router>
+        <Routes />
+    </Router>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { HashRouter as Router, Link, Route } from "react-router-dom";
 import "./index.scss";
-import { Routes } from "./routes/routes";
+import { App } from "./components/App";
 
-ReactDOM.render(
-    <Router>
-        <Routes />
-    </Router>,
-    document.getElementById("app"),
-);
+ReactDOM.render(<App />, document.getElementById("app"));
 
 if ((module as any).hot) {
-    (module as any).hot.accept("./routes/routes", () => {
-        ReactDOM.render(
-            <Router>
-                <Routes />
-            </Router>,
-            document.getElementById("app"),
-        );
+    (module as any).hot.accept("./components/app", () => {
+        ReactDOM.render(<App />, document.getElementById("app"));
     });
 }
