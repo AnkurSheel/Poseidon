5ad992a921317ffdb067f86b37f638c207dde176
Ankur Sheel <ankur.sheel@gmail.com>
2019-04-15 15:11:15 +1200

    wip: 🚧 fix colors for tables

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -1,4 +1,3 @@
-import { props } from "bluebird";
 import * as React from "react";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
@@ -10,7 +9,19 @@ const nullCheckFn = (props: any) => !props.data || props.data.length == 0;
 
 export const Details = (props: any) => (
     <div>
-        <ReactTable data={props.data} columns={props.columns} />
+        <ReactTable
+            data={props.data}
+            columns={props.columns.map((c: any) => ({
+                Header: c.Header,
+                accessor: c.accessor,
+                style: {
+                    color: "white",
+                },
+                headerStyle: {
+                    color: "white",
+                },
+            }))}
+        />
     </div>
 );
 
@@ -18,7 +29,7 @@ export const DetailsWithLoadingIndicator = withLoadingIndicator(Details);
 
 export const withConditionalRenderings: ComponentEnhancer<any, any> = compose(
     withLoadingIndicator,
-    withNullCheck(nullCheckFn),
+    withNullCheck(nullCheckFn)
 );
 
 export const DetailsWithConditionalRenderings = withConditionalRenderings(Details);
