49e1751f37a8cfe8904f99375d682696f50a20c2
Ankur Sheel <ankur.sheel@gmail.com>
2019-04-30 16:12:14 +1200

    Revert "Revert "Navigation drawer""

---
+++ src/Components/list-item-link.tsx
@@ -0,0 +1,23 @@
+import { ListItem, ListItemText } from "@material-ui/core";
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface IListItemLinkProps {
+    to: string;
+    inset?: boolean;
+    text: string;
+    selected: boolean;
+
+    onClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+}
+export const ListItemLink = (props: IListItemLinkProps) => {
+    const renderLink = (itemProps: any) => <Link to={props.to} {...itemProps} />;
+    const { text, inset }: IListItemLinkProps = props;
+    return (
+        <li>
+            <ListItem button component={renderLink} selected={props.selected} onClick={props.onClick}>
+                <ListItemText primary={text} inset={inset} />
+            </ListItem>
+        </li>
+    );
+};
