06d72972b8fc703a7dd4ffc708df6e05f30a5dd2
Ankur Sheel <ankur.sheel@gmail.com>
2019-06-01 10:56:23 +1200

    fix: 🚑 content is compressed

--- src/Components/content.tsx
+++ src/Components/content.tsx
@@ -1,18 +1,26 @@
+import { createStyles, WithStyles } from '@material-ui/core';
 import React, { ReactNode } from 'react';
 import { createStyled } from './createStyled';
+
+const styles = (theme: any) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+    });
+
+const Styled = createStyled(styles);
 
 interface IMainContentProps {
     children: ReactNode;
 }
 
-const Styled = createStyled({
-    root: {
-        flexGrow: 1,
-    },
-});
-
-const Content = (props: IMainContentProps) => {
-    return <Styled>{() => <main>{props.children}</main>}</Styled>;
+const Content = (props: any) => {
+    return (
+        <Styled>
+            {({ classes }: WithStyles<typeof styles>) => <main className={classes.root}>{props.children}</main>}
+        </Styled>
+    );
 };
 
 export default Content;
